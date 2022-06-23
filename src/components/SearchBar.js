4114d1a2222c10e87cd732fcc0c9f40026d32b7d
ndimoforaretas <ndimaret@gmail.com>
2022-06-23 20:30:59 +0200

    Input element refactored to a controlled version

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,23 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+	state = { term: "sdfd" };
+
+	onInputChange = (event) => {
+		this.setState({ term: event.target.value });
+	};
+
 	render() {
 		return (
 			<div className="search-bar ui segment">
 				<form className="ui form">
 					<div className="field">
 						<label>Video Search</label>
-						<input type="text" />
+						<input
+							type="text"
+							value={this.state.term}
+							onChange={this.onInputChange}
+						/>
 					</div>
 				</form>
 			</div>
