042378a385c19ce16125c4238f90f1bcd17b37d6
ndimoforaretas <ndimaret@gmail.com>
2022-06-25 22:32:57 +0200

    video details component implemented

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -7,7 +7,6 @@ const VideoItem = ({ video, onVideoSelect }) => {
 			<img className="ui image" src={video.snippet.thumbnails.medium.url} />
 			<div className="content">
 				<div className="header">{video.snippet.title}</div>
-				<div className="description">{video.snippet.description}</div>
 			</div>
 		</div>
 	);
