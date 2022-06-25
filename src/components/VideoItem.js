82a8e9c121d5400443b4691626565484269c29c7
ndimoforaretas <ndimaret@gmail.com>
2022-06-25 17:28:11 +0200

    rendered list styled with semantic ui

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-const VideoItem = (props) => {
-	return <div>Video Item</div>;
+const VideoItem = ({ video }) => {
+	return (
+		<div className="item">
+			<img className="ui image" src={video.snippet.thumbnails.medium.url} />
+			<div className="content">
+				<div className="header">{video.snippet.title}</div>
+			</div>
+		</div>
+	);
 };
 
 export default VideoItem;
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,7 +2,11 @@ import React from "react";
 import VideoItem from "./VideoItem";
 
 const VideoList = ({ videos }) => {
-	return <div>{videos.length}</div>;
+	const renderedList = videos.map((video) => {
+		return <VideoItem video={video} />;
+	});
+
+	return <div className="ui relaxed divided list">{renderedList}</div>;
 };
 
 export default VideoList;
